ef88e63d68166355284f900ea65a87761211d230
Kabanda Kpanti Michael <you@example.com>
2022-07-21 19:05:25 +0000

    New Porfolio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let tablinks = document.getElementsByClassName('tab_links');
+let tabcontents = document.getElementsByClassName('tab_content');
+
+function openTab(tabname){
+     for(tablink of tablinks){
+        tablink.classList.remove("active_link");
+     }
+     for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active_tab");
+
+     }
+     event.currentTarget.classList.add("active_link");
+     document.getElementById(tabname).classList.add("active_tab");
+}
